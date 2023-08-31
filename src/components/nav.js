e21cb7c307d8276f8dc252170253e0a7160e4aee
BekirLee <nihadbekirli8@gmail.com>
2023-08-31 19:14:03 +0400

    new file for css,bootstrap and jsx in new react file

---
+++ src/components/nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div className="header">
+            <h1>
+                Hello world!
+            </h1>
+        </div>
+    )
+}
+
+export default Navbar;
